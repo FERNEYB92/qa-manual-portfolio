a7adb857183337249608f5bc270541fe3ceeb54e
Ferney Bautista <ferney40@gmail.com>
2026-09-23 23:32:35 -0500

    feat: interaccion con formulario de login usando cy.type y cy.click

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,23 @@
 describe('Proyecto Práctico #5 - Pruebas de Login en Swag Labs', () => {
 
-  it('Debe cargar correctamente la página de inicio de sesión', () => {
-    // Visitar la página web de pruebas
+  beforeEach(() => {
+    // Visitar la página antes de cada prueba
     cy.visit('https://www.saucedemo.com');
+  });
 
-    // Validar que el título principal de la página sea visible
+  it('Debe cargar correctamente la página de inicio de sesión', () => {
     cy.get('.login_logo').should('be.visible');
+  });
+
+  it('Debe interactuar con el formulario e ingresar credenciales válidas', () => {
+    // 1. Escribir el nombre de usuario
+    cy.get('[data-test="username"]').type('standard_user');
+
+    // 2. Escribir la contraseña
+    cy.get('[data-test="password"]').type('secret_sauce');
+
+    // 3. Hacer clic en el botón de Login
+    cy.get('[data-test="login-button"]').click();
   });
 
 });
